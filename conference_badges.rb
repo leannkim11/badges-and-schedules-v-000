1973670e2508212309f19229949d2c195ba9e9bf
Leann Kim <leannminakim@gmail.com>
2018-05-14 23:38:06 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,6 +4,10 @@ def badge_maker (name)
   "Hello, my name is #{name}."
 end
 
+def batch_badge_creator (attendee)
+    "Hello, my name is #{attendee}."
+end
+
 def assign_rooms (names_arr)
   names_arr.each_with_index do|n, index|
     puts "Hello, #{n}! You'll be assigned to room #{index}!"
