f3b584c980a5572cae4f19b0440ebbb1385d1e03
Leann Kim <leannminakim@gmail.com>
2018-05-14 23:36:38 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,9 +1,7 @@
 names_arr = %w[Edsger, Ada, Charles, Alan, Grace, Linus, Matz]
 
-def badge_maker (names_arr)
-  names_arr.each do|x|
-    puts "Hello, my name is #{x}."
-  end
+def badge_maker (name)
+  puts "Hello, my name is #{name}."
 end
 
 def assign_rooms (names_arr)
